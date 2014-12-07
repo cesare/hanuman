14fe099f94a0071940721ae1cc96d4bce6b492a0
SAWADA Tadashi <cesare@mayverse.jp>
2014-12-07 23:33:06 +0900

    Add controller specs

--- spec/controllers/conferences_controller_spec.rb
+++ spec/controllers/conferences_controller_spec.rb
@@ -1,4 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe ConferencesController, type: :controller do
+  describe 'GET #index' do
+    let!(:conference1) { create :conference }
+    let!(:conference2) { create :conference }
+
+    specify do
+      get :index
+
+      expect(response).to be_success
+      expect(response).to render_template :index
+      expect(assigns(:conferences)).to match_array [conference1, conference2]
+    end
+  end
+
+  describe 'GET #show' do
+    let!(:conference) { create :conference }
+
+    specify do
+      get :show, id: conference.id
+
+      expect(response).to be_success
+      expect(response).to render_template :show
+      expect(assigns(:conference)).to eq conference
+    end
+  end
 end
