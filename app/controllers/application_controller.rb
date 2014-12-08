5483c476aa69beba17370a9139b3a1808e7e5bcd
SAWADA Tadashi <cesare@mayverse.jp>
2014-12-08 16:48:50 +0900

    Add methods for person sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,30 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
+  helper_method :current_person
+
   private
 
   def render_not_found
     render file: 'public/404.html', status: :not_found
+  end
+
+  def current_person
+    person_id = session[:person_id]
+    return nil unless person_id
+
+    return @current_person if instance_variable_defined? :@current_person
+
+    @current_person = Person.find_by(id: person_id)
+  end
+
+  def person_signed_in?
+    current_person.present?
+  end
+
+  def authenticate_person!
+    return if current_person.present?
+
+    redirect_to auth_path
   end
 end
