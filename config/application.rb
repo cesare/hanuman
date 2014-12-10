e360168f8a5f5253909863ac2be09d3274d3d69e
SAWADA Tadashi <cesare@mayverse.jp>
2014-12-10 16:49:34 +0900

    Fix the configuration of generators

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,15 @@ Bundler.require(*Rails.groups)
 
 module Hanuman
   class Application < Rails::Application
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.template_engine false
+      g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
+      g.fixture_replacement :factory_girl
+    end
+
     config.time_zone = 'Tokyo'
     config.i18n.available_locales = [:ja]
     config.i18n.default_locale = :ja
