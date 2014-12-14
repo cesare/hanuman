bf7de0c5bea2018f2600f53563e7f107782dc600
SAWADA Tadashi <cesare@mayverse.jp>
2014-12-14 23:54:44 +0900

    Set states and transitions on conferences

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,4 +1,6 @@
 class Conference < ActiveRecord::Base
+  include AASM
+
   has_many :proposals
   has_many :staffs
 
@@ -7,4 +9,30 @@ class Conference < ActiveRecord::Base
   validates :begins_at, presence: true
   validates :ends_at, presence: true
   validates :proposal_deadline, presence: true
+
+  enum state: {
+    draft: 0,
+    open_to_proposals: 10,
+    deciding: 20,
+    settled: 30,
+  }
+
+  aasm column: :state, enum: true do
+    state :draft, initial: true
+    state :open_to_proposals
+    state :deciding
+    state :settled
+
+    event :open do
+      transitions from: :draft, to: :open_to_proposals
+    end
+
+    event :close do
+      transitions from: :open_to_proposals, to: :deciding
+    end
+
+    event :settle do
+      transitions from: :deciding, to: :settled
+    end
+  end
 end
