9ef8a23b93af9183b7a96b12bef21c24fec56bbf
SAWADA Tadashi <cesare@mayverse.jp>
2015-01-11 23:18:13 +0900

    Add email field to people

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150108143127) do
+ActiveRecord::Schema.define(version: 20150111141617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150108143127) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",      default: false, null: false
+    t.string   "email",                      null: false
   end
 
   create_table "proposals", force: :cascade do |t|
